1cb8aafacb498c2170dd571a92367e70d1a98094
I am dead <47627453+I-AmDead@users.noreply.github.com>
2022-03-02 18:14:08 +0700

    Залил шейдеры, конфиги, скрипты для дальнейшей работы

---
+++ gamedata/spawns/acdc_compile.bat
@@ -0,0 +1,2 @@
+perl universal_acdc.pl -compile sources -out new.spawn -g ".." -log log.txt -scan "../config"
+pause
---
+++ gamedata/spawns/acdc_decompile.bat
@@ -0,0 +1,2 @@
+perl universal_acdc.pl -decompile all.spawn -scan ...//config/config -out sources -g ".." -log log.txt
+pause
