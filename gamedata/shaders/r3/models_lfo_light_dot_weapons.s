4ff0b702f286c78a4fda70941a059fc5071cee63
I am dead <47627453+I-AmDead@users.noreply.github.com>
2022-03-05 00:30:24 +0700

    Шейдеры динамических коллиматоров, добавил недостающие шейдеры для р2

--- gamedata/shaders/r3/models_lfo_light_dot_weapons.s
+++ gamedata/shaders/r3/models_lfo_light_dot_weapons.s
@@ -7,5 +7,4 @@ function normal   (shader, t_base, t_second, t_detail)
       : sorting	  	(2,true)
       : distort   	(true)
 	shader:dx10texture	("s_base",	t_base)
-	shader:dx10sampler	("smp_base")
 end
