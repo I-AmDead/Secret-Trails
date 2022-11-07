7a498a92872da2e3e1ccc1877c11605bae0ec4eb
I am dead <47627453+I-AmDead@users.noreply.github.com>
2022-11-08 05:21:37 +0700

    Правки шейдеров

--- gamedata/shaders/r3/settings_screenspace_PUDDLES.h
+++ gamedata/shaders/r3/settings_screenspace_PUDDLES.h
@@ -4,9 +4,9 @@
 
 #define G_PUDDLES_GLOBAL_SIZE				1.0f	// Puddles global size. ( This affect distance and puddles size )
 #define G_PUDDLES_SIZE						0.8f	// Puddles individual size. ( This only affect puddles size )
-#define G_PUDDLES_BORDER_HARDNESS			0.8f	// Border hardness. ( 1.0f = Extremely defined border )
-#define G_PUDDLES_TERRAIN_EXTRA_WETNESS		1.1f	// Terrain extra wetness when raining. ( Over time like puddles )
-#define G_PUDDLES_REFLECTIVITY				0.6f	// Reflectivity. ( 1.0f = Mirror like )
+#define G_PUDDLES_BORDER_HARDNESS			0.7f	// Border hardness. ( 1.0f = Extremely defined border )
+#define G_PUDDLES_TERRAIN_EXTRA_WETNESS		0.15f	// Terrain extra wetness when raining. ( Over time like puddles )
+#define G_PUDDLES_REFLECTIVITY				0.4f	// Reflectivity. ( 1.0f = Mirror like )
 #define G_PUDDLES_TINT						float3(0.65f, 0.6f, 0.6f) // Puddles tint RGB.
 
 #define G_PUDDLES_RIPPLES							// Comment to disable ripples
