d35da6c0477a9fe5d79a68cf6ee8b0afc02e5319
I-AmDead <47627453+I-AmDead@users.noreply.github.com>
2023-11-22 17:53:30 +0700

    .

--- gamedata/shaders/r3/models_watch.s
+++ gamedata/shaders/r3/models_watch.s
@@ -1,21 +1,22 @@
 function normal		(shader, t_base, t_second, t_detail)
-	shader:begin	("models_watch","models_watch")
-		: fog			(true)
-		: distort		(true)
-		: emissive 	(true)
+	shader:begin	("deffer_model_flat","models_watch")
+			: fog		(false)
+			: emissive 	(true)
+--	shader:sampler	("s_base")      :texture	(t_base)
 	shader:dx10texture	("s_base",	t_base)
-	shader:dx10texture	("s_hud_rain",	"fx\\hud_rain")
 	shader:dx10sampler	("smp_base")
-	shader:dx10stencil	(true, cmp_func.always, 255 , 127, stencil_op.keep, stencil_op.replace, stencil_op.keep)
+	shader:dx10stencil	( 	true, cmp_func.always, 
+							255 , 127, 
+							stencil_op.keep, stencil_op.replace, stencil_op.keep)
 	shader:dx10stencil_ref	(1)
+	
+	--shader: dx10color_write_enable( true, true, true, false)
 end
 
 function l_special	(shader, t_base, t_second, t_detail)
-	shader:begin	("deffer_model_flat",	"accum_watch")
+	shader:begin	("shadow_direct_model",	"accum_watch")
 			: zb 		(true,false)
 			: fog		(false)
 			: emissive 	(true)
 
-	shader:dx10texture	("s_base",	t_base)
-	shader:dx10sampler	("smp_base")
 end
