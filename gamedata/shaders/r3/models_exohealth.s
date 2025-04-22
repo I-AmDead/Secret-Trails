0f24a9194473cbb03538c06b9fcd6e16513360b8
I am dead <47627453+I-AmDead@users.noreply.github.com>
2025-04-22 22:26:36 +0700

    Отрубил distortion там где он не нужен
    Один хрен он бы не работал, нету четвертого элемента

--- gamedata/shaders/r3/models_exohealth.s
+++ gamedata/shaders/r3/models_exohealth.s
@@ -5,7 +5,6 @@ function normal(shader, t_base, t_second, t_detail)
       : blend		(true,blend.srcalpha,blend.invsrcalpha)
       : aref		(true,0)
       : sorting		(2,true)
-      : distort		(true)
 	shader:dx10texture	("s_base",	t_base)
 	shader:dx10sampler	("smp_base")	
 end
--- gamedata/shaders/r3/models_transparent.s
+++ gamedata/shaders/r3/models_transparent.s
@@ -11,16 +11,3 @@ function normal   (shader, t_base, t_second, t_detail)
 	shader: dx10sampler ("smp_base")
 	shader: dx10sampler ("smp_rtlinear")
 end
-
-function l_special  (shader, t_base, t_second, t_detail)
-  shader:begin  ("model_distort4glass","models_distort")
-      : sorting   (3, true)
-      : blend     (true,blend.srcalpha,blend.invsrcalpha)
-      : zb        (true,false)
-      : fog       (false)
-      : distort   (true)
-	shader: dx10texture ("s_base", t_base)
-	shader: dx10texture ("s_distort", "pfx\\pfx_dist_glass")
-
-	shader: dx10sampler ("smp_linear")
-end
