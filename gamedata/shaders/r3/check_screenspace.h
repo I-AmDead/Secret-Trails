a9c104c2b32437dfbd7b99dcc45722f2188ed04e
I am dead <47627453+I-AmDead@users.noreply.github.com>
2025-05-07 14:27:06 +0700

    Управляется с движка

--- gamedata/shaders/r3/check_screenspace.h
+++ gamedata/shaders/r3/check_screenspace.h
@@ -4,7 +4,6 @@
 #include "check_screenspace_SSR.h"
 #include "check_screenspace_AO.h"
 #include "check_screenspace_NVG.h"
-#include "check_screenspace_DEBAND.h"
 #include "check_screenspace_FOG.h"
 #include "check_screenspace_FLORA.h"
 #include "check_screenspace_INTER_GRASS.h"
