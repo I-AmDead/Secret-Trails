ae2a0a6481832a35883c96751bd768852e47679d
I am dead <47627453+I-AmDead@users.noreply.github.com>
2025-08-05 20:29:44 +0700

    На тест шейдер стекла из SSS 23

--- gamedata/shaders/r3/models_lfo_light_dot_weapons.s
+++ gamedata/shaders/r3/models_lfo_light_dot_weapons.s
@@ -4,7 +4,7 @@ function normal(shader, t_base, t_second, t_detail)
       : zb(true,false)
       : blend(true,blend.srcalpha,blend.invsrcalpha)
       : aref(true,0)
-      : sorting(2,true)
+      : sorting(3,true)
     shader:dx10texture("s_base", t_base)
     shader:dx10sampler("smp_rtlinear")
 end
