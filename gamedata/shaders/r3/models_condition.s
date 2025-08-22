558455f49762efa111e09a035c26516676575806
I am dead <47627453+I-AmDead@users.noreply.github.com>
2025-08-22 22:00:06 +0700

    scope_pass

--- gamedata/shaders/r3/models_condition.s
+++ gamedata/shaders/r3/models_condition.s
@@ -5,6 +5,7 @@ function normal(shader, t_base, t_second, t_detail)
       : blend   	(true,blend.srcalpha,blend.one)
       : aref    	(true,0)
       : sorting		(2, true)
+      : scope_pass	(true)
 	shader:dx10texture("s_base", t_base)
 	shader:dx10sampler("smp_base")
 end
