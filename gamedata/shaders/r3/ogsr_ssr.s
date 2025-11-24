0c9ffc9d1774d0ed5501c0f8c35e79c426987172
I am dead <47627453+I-AmDead@users.noreply.github.com>
2025-11-25 05:40:19 +0700

    Выкинул размытие у SSR
    Чуть упростил копирование убрав дроуколлы

--- gamedata/shaders/r3/ogsr_ssr.s
+++ gamedata/shaders/r3/ogsr_ssr.s
@@ -4,7 +4,7 @@ function element_0(shader, t_base, t_second, t_detail)
 		:zb(false, false)
 	shader:dx10texture("s_position", "$user$position")
 	shader:dx10texture("s_diffuse", "$user$albedo")
-	shader:dx10texture("ssr_image", "$user$ssr")
+	shader:dx10texture("ssr_image", "$user$ssr1")
 	shader:dx10texture("s_rimage", "$user$generic_temp")
 	shader:dx10texture("s_vollight", "$user$generic2")
 	shader:dx10texture("env_s0", "$user$env_s0")
@@ -17,43 +17,11 @@ function element_0(shader, t_base, t_second, t_detail)
 end
 
 function element_1(shader, t_base, t_second, t_detail)
-	shader:begin("stub_screen_space", "ogsr_ssr_blur")
-		:fog(false)
-		:zb(false, false)
-	shader:dx10texture("ssr_image", "$user$ssr_temp_2")
-	shader:dx10sampler("smp_nofilter")
-end
-
-function element_2(shader, t_base, t_second, t_detail)
-	shader:begin("stub_screen_space", "ogsr_ssr_blur")
-		:fog(false)
-		:zb(false, false)
-	shader:dx10texture("ssr_image", "$user$ssr_temp_1")
-	shader:dx10sampler("smp_nofilter")
-end
-
-function element_3(shader, t_base, t_second, t_detail)
 	shader:begin("stub_screen_space", "ogsr_ssr_combine")
 		:fog(false)
 		:zb(false, false)
 	shader:dx10texture("s_rimage", "$user$generic_temp")
-	shader:dx10texture("ssr_image", "$user$ssr_temp_2")
+	shader:dx10texture("ssr_image", "$user$ssr2")
 	shader:dx10sampler("smp_nofilter")
 	shader:dx10sampler("smp_linear")
-end
-
-function element_4(shader, t_base, t_second, t_detail)
-	shader:begin("stub_screen_space", "ogsr_ssr_noblur")
-		:fog(false)
-		:zb(false, false)
-	shader:dx10texture("ssr_image", "$user$ssr")
-	shader:dx10sampler("smp_nofilter")
-end
-
-function element_5(shader, t_base, t_second, t_detail)
-	shader:begin("stub_screen_space", "ogsr_ssr_noblur")
-		:fog(false)
-		:zb(false, false)
-	shader:dx10texture("ssr_image", "$user$ssr_temp_2")
-	shader:dx10sampler("smp_nofilter")
 end
