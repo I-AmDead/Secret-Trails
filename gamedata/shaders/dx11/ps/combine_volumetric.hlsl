d4211a2b29f96bb98b960b92af32b0fc05471b5f
I am dead <47627453+I-AmDead@users.noreply.github.com>
2026-01-19 21:07:03 +0700

    Не нужно
    Все равно этот шум сглаживание похерит

--- gamedata/shaders/dx11/ps/combine_volumetric.hlsl
+++ gamedata/shaders/dx11/ps/combine_volumetric.hlsl
@@ -1,6 +1,5 @@
 #include "common\common.h"
 
-Texture2D noise_tex;
 Texture2D s_vollight1;
 Texture2D s_vollight2;
 
@@ -15,9 +14,6 @@ float4 main(p_screen I) : SV_Target
     // Noise TC
     float2 uv_noise = I.tc0.xy;
     uv_noise.x *= screen_res.x / screen_res.y;
-
-    // Add noise to dither the result
-    color = saturate(color - noise_tex.Sample(smp_linear, uv_noise * 2.5).xxxx * 0.01f);
 
     // Discard Sky.
     color *= depth < 0.001 ? 1.0f : saturate(depth * 1.5f);
