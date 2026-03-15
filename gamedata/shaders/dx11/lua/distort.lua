edb504863ce32ec6a2e0b9c7edf750fa140ed959
I am dead <47627453+I-AmDead@users.noreply.github.com>
2026-03-15 08:16:07 +0700

    Delete distort shader

--- gamedata/shaders/dx11/lua/distort.lua
+++
@@ -1,9 +0,0 @@
-function normal(shader, t_base, t_second, t_detail)
-	shader:begin("stub_notransform_t", "distort")
-		:fog(false)
-		:zb(false,false)
-	shader:dx10texture("s_base", "$user$backbuffer")
-	shader:dx10texture("s_distort", "$user$distortion")
-
-	shader:dx10sampler("smp_rtlinear");
-end
