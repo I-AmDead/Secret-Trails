16bdb5f98215eb1490bd95cc93a369d5a76a0445
I am dead <47627453+I-AmDead@users.noreply.github.com>
2026-04-07 18:31:08 +0700

    Пусть количество фильтров само вычисляется

--- gamedata/shaders/dx11/common/screenspace/screenspace_lut.h
+++ gamedata/shaders/dx11/common/screenspace/screenspace_lut.h
@@ -6,22 +6,33 @@
  * @ Mod: https://www.moddb.com/mods/stalker-anomaly/addons/screen-space-shaders
  */
 
-// Settings
-#include "common\screenspace\settings_screenspace_LUT.h"
-
-// Internal --
-#define LUT_GROUPS max(1, G_CELLS_GROUPS)
-
-#define TEXEL_SIZE float2(1.0f / G_LUT_SIZE_W, 1.0f / (G_CELLS_SIZE * LUT_GROUPS))
-#define TEXEL_HALF float2(TEXEL_SIZE.xy / 2.0f)
-#define TEXEL_FIX TEXEL_SIZE.y * LUT_GROUPS
+#define G_ADVANCE_TRANSITION
+#define G_CELLS_SIZE 32 // Width/Height of your cell
 
 Texture2D s_lut_atlas;
 
 uniform float4 lut_params;
 
+float3 sample_lut_group(float4 uvs, float lut_frac, float group_offset, int groups)
+{
+    float2 grp_offset = float2(0.0, group_offset / groups);
+    return lerp(s_lut_atlas.Sample(smp_linear, uvs.xy + grp_offset).rgb, s_lut_atlas.Sample(smp_linear, uvs.zw + grp_offset).rgb, lut_frac);
+}
+
 float3 img_lut(float3 base_col)
 {
+    // Get Texture Dimension
+    int G_LUT_SIZE_W, G_LUT_SIZE_H, G_LUT_GROUPS;
+    s_lut_atlas.GetDimensions(G_LUT_SIZE_W, G_LUT_SIZE_H);
+
+    // Get number lut groups
+    G_LUT_GROUPS = G_LUT_SIZE_H / G_CELLS_SIZE;
+
+    // Internal
+    float2 TEXEL_SIZE = float2(1.0f / G_LUT_SIZE_W, 1.0f / (G_CELLS_SIZE * G_LUT_GROUPS));
+    float2 TEXEL_HALF = float2(TEXEL_SIZE.xy / 2.0f);
+    float TEXEL_FIX = TEXEL_SIZE.y * G_LUT_GROUPS;
+
     // Prepare LUT UVs
     float3 cells = base_col * G_CELLS_SIZE - base_col;
     float lut_frac = frac(cells.b);
@@ -31,22 +42,14 @@ float3 img_lut(float3 base_col)
     // Final LUT UVs
     float4 uvs = float4(cells.rg, cells.r + TEXEL_FIX, cells.g);
 
-    // Group offset
-    float2 grp_offset = float2(0.0, lut_params.y / LUT_GROUPS);
-
-    // Sample LUTs
-    float3 lut_col = lerp(s_lut_atlas.Sample(smp_linear, uvs.xy + grp_offset).rgb, s_lut_atlas.Sample(smp_linear, uvs.zw + grp_offset).rgb, lut_frac);
+    // Sample LUT
+    float3 lut_col = sample_lut_group(uvs, lut_frac, lut_params.y, G_LUT_GROUPS);
 
 #ifdef G_ADVANCE_TRANSITION
-
-    // Group2 offset
-    grp_offset = float2(0.0, lut_params.z / LUT_GROUPS);
-
-    // Sample transition LUTs
-    float3 SecondLUT = lerp(s_lut_atlas.Sample(smp_linear, uvs.xy + grp_offset).rgb, s_lut_atlas.Sample(smp_linear, uvs.zw + grp_offset).rgb, lut_frac);
-
-    lut_col = lerp(lut_col, SecondLUT, lut_params.w);
+    // Sample LUT
+    float3 second_lut = sample_lut_group(uvs, lut_frac, lut_params.z, G_LUT_GROUPS);
+    lut_col = lerp(lut_col, second_lut, lut_params.w);
 #endif
 
-    return lerp(base_col.rgb, lut_col.rgb, lut_params.x);
+    return lerp(base_col, lut_col, lut_params.x);
 }
--- gamedata/shaders/dx11/common/screenspace/settings_screenspace_LUT.h
+++
@@ -1,9 +0,0 @@
-// [ SETTINGS ] [ LUT ]
-
-#define G_LUT_SIZE_W 1024.0 // Width of your LUT texture
-#define G_LUT_SIZE_H 544.0 // Width of your LUT texture
-
-#define G_CELLS_SIZE 32 // Width/Height of your cell
-#define G_CELLS_GROUPS G_LUT_SIZE_H / G_CELLS_SIZE // Quantity of tables in your texture
-
-#define G_ADVANCE_TRANSITION // Note for Modders: You can enable this option to do smooth transitions between tables in realtime. Check the script
